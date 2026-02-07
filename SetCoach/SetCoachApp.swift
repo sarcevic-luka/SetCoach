d35ef1f87c7a26f83c2a6123e1876ec410e4dc5c
Luka Sarcevic <lukasarcevic.business@gmail.com>
2026-02-08 00:35:36 +0100

    Initial Commit

---
+++ SetCoach/SetCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetCoachApp.swift
+//  SetCoach
+//
+//  Created by Code Forge on 08.02.2026..
+//
+
+import SwiftUI
+
+@main
+struct SetCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
